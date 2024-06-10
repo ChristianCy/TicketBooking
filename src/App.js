594d138158bd6f9d3355bb082d81e2a1c329e01d
ChristianCy <125923280+ChristianCy@users.noreply.github.com>
2024-06-10 18:46:58 +0100

    Update to the home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home.jsx';
-import List from  './pages/list/List.jsx';
 import Buses from  './pages/buses/Buses.jsx';
 
 
@@ -10,8 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/buses" element={<List />} />
-        <Route path="/buses/:id" element={<Buses />} />
+        <Route path="/buses" element={<Buses />} />
       </Routes>
     </Router>
   );
--- src/components/Headerr/header.jsx
+++ src/components/Headerr/header.jsx
@@ -1,24 +1,48 @@
+import React from 'react';
 import './headerr.css'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBus, faCar } from '@fortawesome/free-solid-svg-icons';
+import { faBus, faCar, faSuitcaseRolling, faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
+  const handleBusClick = () => {
+    // Handle Bus Trips click
+    console.log('Bus Trips clicked');
+  };
+
+  const handleCarClick = () => {
+    // Handle Car Rental click
+    console.log('Car Rental clicked');
+  };
+
   return (
     <div className="Header">
       <div className="headerContainer">
         <div className="headerList">
-          <div className="headerListItem active">
+          <button className="headerListItem active" onClick={handleBusClick}>
             <FontAwesomeIcon icon={faBus} />
             <span>Book Bus Trips</span>
-          </div>
-          <div className="headerListItem active">
+          </button>
+          <button className="headerListItem active" onClick={handleCarClick}>
             <FontAwesomeIcon icon={faCar} />
             <span>Car Rental Service</span>
-          </div>
+          </button>
         </div> 
         <h1 className="headerTitle">Book Your Bus Tickets and Car Rentals at the Best Prices</h1>
         <p className="headerDesc">Choose the best option that suits your needs and budget!</p>
-        <button className="headerBt"> Sign in / Register</button>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faSuitcaseRolling} className="headerIcon" />
+            <input type="text" placeholder="Where are you going?" className="headerSearchInput" />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">Date to Date</span>
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn2">Search</button>
+          </div>
+        </div>
       </div>
     </div>
   );
