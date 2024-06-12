12bd86e684baf12f669ec8cac7b252cb25ef7e66
ChristianCy <125923280+ChristianCy@users.noreply.github.com>
2024-06-12 16:35:35 +0100

    Update

--- src/pages/PvCar/pvcar.jsx
+++ src/pages/PvCar/pvcar.jsx
@@ -8,12 +8,12 @@ import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 function PvCar() {
   const photos = [
-    { src: 'https://example.com/photo1.jpg' },
-    { src: 'https://example.com/photo2.jpg' },
-    { src: 'https://example.com/photo3.jpg' },
-    { src: 'https://example.com/photo4.jpg' },
-    { src: 'https://example.com/photo5.jpg' },
-    { src: 'https://example.com/photo6.jpg' },
+    { src: 'https://platform.cstatic-images.com/xlarge/in/v2/261d6b78-f6bc-58a8-bbed-04fbc869539d/9d7ce0ff-fe25-486e-b285-1e827ad50fd9/FGopzIMwqAPcEw6suRmv_I8xNx0.jpg' },
+    { src: 'https://platform.cstatic-images.com/xlarge/in/v2/261d6b78-f6bc-58a8-bbed-04fbc869539d/9d7ce0ff-fe25-486e-b285-1e827ad50fd9/IT0pmULThwnoVrwMzUC0aFnKzZs.jpg' },
+    { src: 'https://platform.cstatic-images.com/xlarge/in/v2/261d6b78-f6bc-58a8-bbed-04fbc869539d/9d7ce0ff-fe25-486e-b285-1e827ad50fd9/kibJeRXTdwyiO6aCt_JzqpDY3gw.jpg' },
+    { src: 'https://platform.cstatic-images.com/xlarge/in/v2/261d6b78-f6bc-58a8-bbed-04fbc869539d/9d7ce0ff-fe25-486e-b285-1e827ad50fd9/kibJeRXTdwyiO6aCt_JzqpDY3gw.jpg' },
+    { src: 'https://platform.cstatic-images.com/xlarge/in/v2/261d6b78-f6bc-58a8-bbed-04fbc869539d/9d7ce0ff-fe25-486e-b285-1e827ad50fd9/tqWcHrmspwqD_wIfL9gwSu9GXfE.jpg' },
+    { src: 'https://platform.cstatic-images.com/xlarge/in/v2/261d6b78-f6bc-58a8-bbed-04fbc869539d/9d7ce0ff-fe25-486e-b285-1e827ad50fd9/nha_Bt8u2CpWcrRjXiiRbUAnEy8.jpg' },
   ];
 
   return (
@@ -33,10 +33,25 @@ function PvCar() {
           </span>
           <div className="pvCarImages">
             {photos.map(photo => (
-              <div className="pvCarImgWrapper" >
+              <div className="pvCarImgWrapper">
                 <img src={photo.src} alt="" className="pvCarImg" />
               </div>
             ))}
+          </div>
+          <div className="pvCarDetails">
+              
+          </div>
+          <div className="pvCarDetailPrice">
+            <h1>Perfect for off track journeys</h1>
+            <span>
+              Exellent score of 10
+
+            </span>
+            <h2>
+              <b>50000 FCFA </b> per day
+            </h2>
+            <button>Rent Now!</button>
+
           </div>
         </div>
       </div>
